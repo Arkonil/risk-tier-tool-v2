719547cba4f962bf19edb5d8b1ecf55dfe4d22d4
Arkonil <arkonil0904@gmail.com>
2025-06-30 19:15:20 +0000

    refactor: change selected_iterations from list to tuple for consistency

--- classes/home_page_state.py
+++ classes/home_page_state.py
@@ -10,8 +10,8 @@ class HomePageState:
         self.metrics = DefaultOptions().default_metrics
         self.comparison_mode = False
         self.selected_iterations: list[tuple[str, bool]] = [
-            [None, None],
-            [None, None],
+            (None, None),
+            (None, None),
         ]
         self.comparison_view_mode: t.Literal["column", "row"] = "column"
 

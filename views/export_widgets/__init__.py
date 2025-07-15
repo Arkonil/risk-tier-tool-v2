f05517316a05f94f4b286797661275fb2c08c98e
Arkonil <arkonil0904@gmail.com>
2025-07-15 18:25:57 +0000

    fix: show only session archive export if no iterations are created

--- views/export_widgets/__init__.py
+++ views/export_widgets/__init__.py
@@ -1,12 +1,20 @@
 import streamlit as st
 
+from classes.session import Session
 from views.export_widgets.session_archive import session_archive_download_widget
 from views.export_widgets.sas_code import sas_code_generator_widget
 from views.export_widgets.python_code import python_code_generator_widget
 
 
 def export_view():
+    session: Session = st.session_state["session"]
+    iteration_graph = session.iteration_graph
+
     st.title("Export")
+
+    if iteration_graph.is_empty:
+        session_archive_download_widget()
+        return
 
     export_archive, export_python, export_sas = st.tabs([
         "Session Archive",
